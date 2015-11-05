a8aa12faf3531e8c9da42bcb7c1e6edf440ea58d
dickeyxxx <jeff@dickeyxxx.com>
2015-11-04 17:23:30 -0800

    added postInstallHelp

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+var cli         = require('heroku-cli-util');
 var extend      = require('util')._extend;
 var command     = require('./commands/pt');
 var description = "Search and/or tail logs";
@@ -11,3 +12,15 @@ exports.commands = [
   extend({ topic: "pt",         description: description }, command),
   extend({ topic: "papertrail", description: description }, command)
 ];
+
+exports.postInstallHelp = `
+Use heroku-papertrail with ${cli.color.cyan('heroku papertrail')}:
+  $ heroku papertrail -a myapp
+  <logs>
+
+Filter the logs:
+  $ heroku papertrail -a myapp R14
+
+Tail the logs:
+  $ heroku papertrail -a myapp --tail
+`;
